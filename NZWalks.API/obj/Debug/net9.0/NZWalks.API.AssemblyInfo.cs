28fb18212e4a16a8a3e5c07494564644b79b3ab9
Joseph Lodico <josephl@addatech.ca>
2026-06-15 11:42:40 -0400

    - Creating Walks Controller and Create New Walk Action Method - Tested it with swagger and it works perfectly fine.

--- NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c4587a165b0d1560c3e09be407b818855c1454")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
