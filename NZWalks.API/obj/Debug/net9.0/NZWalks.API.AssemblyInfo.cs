73b3608ea359df1e6d3e83d5a92e4fd0113ac334
Joseph Lodico <josephl@addatech.ca>
2026-06-17 14:27:48 -0400

    Section 8 of the ASP.NET Course - Setted up the Authentication - Install Nuget Packages - Tested Authentication Without JWT Token

--- NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540b46773a0505f62091ba30b40721f8d8784525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21e6300d97412e76eeab78d9e54c2155612c536")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
