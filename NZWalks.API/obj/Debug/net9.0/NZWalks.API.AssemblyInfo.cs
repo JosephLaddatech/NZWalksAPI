2d1d2d4e56570addcb1bdce8025c1fa9eafbc7ac
Joseph Lodico <josephl@addatech.ca>
2026-06-18 13:12:24 -0400

    - Created Auth Controller and Register Action Method - Created Login Action method

--- NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b3608ea359df1e6d3e83d5a92e4fd0113ac334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f5d3af28fdc6b961a21ea1be17074887533a67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
