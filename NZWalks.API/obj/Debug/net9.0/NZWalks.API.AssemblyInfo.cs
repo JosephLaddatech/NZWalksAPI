9b9c983e9da6b05e48258d9c41a525faa48531c0
Joseph Lodico <josephl@addatech.ca>
2026-06-19 14:44:37 -0400

    - Serving Static Files Through ASPNET Core Web API.

--- NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net9.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad6b94e647a91e297f2b778f978d31e305bda707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79344d6a4779e44437acc7cc30e205e8d753b588")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
