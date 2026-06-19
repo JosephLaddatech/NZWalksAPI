9ebd718a9986299319c7903b6caf57157272ae4f
Joseph Lodico <josephl@addatech.ca>
2026-06-19 16:32:25 -0400

    - Section 10 of the ASP.NET Course.

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Section 10 in the ASP.NET Course
 var logger = new LoggerConfiguration()
     .WriteTo.Console()
     .MinimumLevel.Warning()
